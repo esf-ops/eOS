612d90d801ee0178b4660b685a7a8f30623ef4b2
esf-ops <chris.henely@elitestonefabrication.com>
2026-06-17 19:33:47 -0500

    Polish Home Launcher premium UI

--- app-home/src/main.tsx
+++ app-home/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./ui/App";
+import "../../shared/eliteos-ui/eliteosTokens.css";
 import "./ui/styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
