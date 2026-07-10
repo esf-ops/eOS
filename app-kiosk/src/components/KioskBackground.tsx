41bcc67bda4c5dc54e36e0412ef066060deb1a6d
esf-ops <chris.henely@elitestonefabrication.com>
2026-07-10 09:14:16 -0500

    Add premium kiosk home animations

--- app-kiosk/src/components/KioskBackground.tsx
+++ app-kiosk/src/components/KioskBackground.tsx
@@ -8,6 +8,8 @@ export function KioskBackground() {
       <div className="kiosk-bg-orb kiosk-bg-orb-1" />
       <div className="kiosk-bg-orb kiosk-bg-orb-2" />
       <div className="kiosk-bg-orb kiosk-bg-orb-3" />
+      {/* Fourth orb: subtle warm ember at lower-right quadrant adds depth */}
+      <div className="kiosk-bg-orb kiosk-bg-orb-4" />
       <div className="kiosk-bg-veins" />
       <div className="kiosk-bg-grain" />
     </div>
